9a7f577dd84a5c2bc5e8249ef7e11da3a1ccb5d1
PuneethReddyV <vpuneeth007@gmail.com>
2015-04-06 18:10:21 +0530

    Update StringExercises.java
    Thanks for sharing knowledge.

--- src/main/java/com/insightfullogic/java8/answers/chapter3/StringExercises.java
+++ src/main/java/com/insightfullogic/java8/answers/chapter3/StringExercises.java
@@ -18,5 +18,18 @@ public class StringExercises {
         return strings.stream()
                       .max(Comparator.comparing(StringExercises::countLowercaseLetters));
     }
+    
+    public static void mostLowercaseString(List<String> strings) {
+        int requiredStringCount = list1.stream().mapToInt(action->{
+				
+				return (int) action.chars().filter(Character::isLowerCase).count();
+				
+			}).max().getAsInt();
+	list1.stream().forEach(reqStr -> {
+			if(reqStr.chars().filter(Character::isLowerCase).count()==requiredStringCount){
+				System.out.println(reqStr+" Have highest number of strings "+requiredStringCount);
+			}
+	});
+    }
 
 }
